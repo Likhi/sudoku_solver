69f0961d75a26f07e62712582c48c3458a695173
Likhi <likhi.ondov@gmail.com>
2021-03-18 15:58:17 -0700

    playtime i guess
    added test images to experiment with Tesseract-OCR

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from sudoku import solve_all
+import os,time
 
 if __name__ == "__main__":
 
@@ -6,4 +7,17 @@ if __name__ == "__main__":
     grid2  = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
     hard1  = '.....6....59.....82....8....45........3........6..3.54...325..6..................'
 
-    solve_all([grid2])
+    solve_all([hard1])
+
+    # Tesseract-OCR testing
+    image = "eurotext.png"
+    output = "output"
+    options = ""
+    command = "tesseract images\\%s %s %s" % (image,output,options)
+
+    os.system(command)
+    time.sleep(2) # let output.txt get regenerated
+
+    f=open("%s.txt" % output,"r",encoding="utf8")
+    print(f.read())
+    f.close()
